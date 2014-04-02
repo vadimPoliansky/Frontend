b40efdcf8d2903c9055966ec0e3676d9d0bfcdae
vadimPoliansky <vadimPoliansky@gmail.com>
2014-04-02 14:03:54 -0400

    Security Test 2

--- Test1/Global.asax.cs
+++ Test1/Global.asax.cs
@@ -29,5 +29,11 @@ namespace IndInv
             GlobalConfiguration.Configuration.Filters.Add(new System.Web.Http.AuthorizeAttribute());
         }
 
+        protected void Application_BeginRequest()
+        {
+            if (!Context.Request.IsSecureConnection)
+                Response.Redirect(Context.Request.Url.ToString().Replace
+                ("http:", "https:"));
+        }
     }
 }
