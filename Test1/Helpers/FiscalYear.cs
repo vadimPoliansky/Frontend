691e3ce8396f96bb47be0100dbf6b3e52aa09fbb
Vadim Poliansky <vadimpoliansky@gmail.com>
2014-07-25 13:16:33 -0400

    nv T0.57

--- Test1/Helpers/FiscalYear.cs
+++ Test1/Helpers/FiscalYear.cs
@@ -51,5 +51,29 @@ namespace IndInv.Helpers
                     break;
             }
         }
+
+        public static string FYStrFullFormated(string fyStr, int fiscalYear)
+        {
+            switch (fyStr)
+            {
+                case "FY_3":
+                    return "FY20" + (FYInt(fiscalYear) + 1 - 3).ToString();
+                    break;
+                case "FY_2":
+                    return "FY_" + (FYInt(fiscalYear) - 2).ToString() + "_" + (FYInt(fiscalYear) + 1 - 2).ToString() + "_YTD";
+                    break;
+                case "FY_1":
+                    return "FY_" + (FYInt(fiscalYear) - 1).ToString() + "_" + (FYInt(fiscalYear) + 1 - 1).ToString() + "_YTD";
+                    break;
+                case "FY_Header":
+                    return "FY " + (FYInt(fiscalYear)).ToString() + "/" + (FYInt(fiscalYear) + 1).ToString() + " Performance";
+                default:
+                    return fyStr.Replace(
+                        "FY_",
+                        "FY_" + (FYInt(fiscalYear)).ToString() + "_" + (FYInt(fiscalYear) + 1).ToString() + "_"
+                        );
+                    break;
+            }
+        }
     }
 }
