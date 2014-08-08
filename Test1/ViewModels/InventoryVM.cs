0a98830475e6b918287630828a521e9b6af3b378
Vadim Poliansky <vadimpoliansky@gmail.com>
2014-08-08 10:31:32 -0400

    nv T0.821

--- Test1/ViewModels/InventoryVM.cs
+++ Test1/ViewModels/InventoryVM.cs
@@ -27,33 +27,33 @@ namespace IndInv.Models.ViewModels
         public string FY_Q4_Sup { get; set; }
         public string FY_YTD { get; set; }
         public string FY_YTD_Sup { get; set; }
-        public string Target { get; set; }
-        public string Target_Sup { get; set; }
-        public string Comparator { get; set; }
-        public string Comparator_Sup { get; set; }
-        public string Performance_Threshold { get; set; }
-        public string Performance_Threshold_Sup { get; set; }
+        public string FY_Target { get; set; }
+        public string FY_Target_Sup { get; set; }
+        public string FY_Comparator { get; set; }
+        public string FY_Comparator_Sup { get; set; }
+        public string FY_Performance_Threshold { get; set; }
+        public string FY_Performance_Threshold_Sup { get; set; }
 
-        public Int16 Colour_ID { get; set; }
-        public string Custom_YTD { get; set; }
-        public string Custom_Q1 { get; set; }
-        public string Custom_Q2 { get; set; }
-        public string Custom_Q3 { get; set; }
-        public string Custom_Q4 { get; set; }
+        public Int16 FY_Colour_ID { get; set; }
+        public string FY_Custom_YTD { get; set; }
+        public string FY_Custom_Q1 { get; set; }
+        public string FY_Custom_Q2 { get; set; }
+        public string FY_Custom_Q3 { get; set; }
+        public string FY_Custom_Q4 { get; set; }
 
-        public string Definition_Calculation { get; set; }
-        public string Target_Rationale { get; set; }
-        public string Comparator_Source { get; set; }
+        public string FY_Definition_Calculation { get; set; }
+        public string FY_Target_Rationale { get; set; }
+        public string FY_Comparator_Source { get; set; }
 
-        public string Data_Source_MSH { get; set; }
-        public string Data_Source_Benchmark { get; set; }
-        public string OPEO_Lead { get; set; }
+        public string FY_Data_Source_MSH { get; set; }
+        public string FY_Data_Source_Benchmark { get; set; }
+        public string FY_OPEO_Lead { get; set; }
 
-        public string Q1_Color { get; set; }
-        public string Q2_Color { get; set; }
-        public string Q3_Color { get; set; }
-        public string Q4_Color { get; set; }
-        public string YTD_Color { get; set; }
+        public string FY_Q1_Color { get; set; }
+        public string FY_Q2_Color { get; set; }
+        public string FY_Q3_Color { get; set; }
+        public string FY_Q4_Color { get; set; }
+        public string FY_YTD_Color { get; set; }
 
         public int Fiscal_Year { get; set; }
 
